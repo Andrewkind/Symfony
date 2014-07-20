455445fba5f959dc9fd753ffde32f9b4605d20d0
Andrew Kind <Andrew.w.kind@gmail.com>
2014-07-20 05:14:43 -0600

    Change controller to template

--- learnsymfony/src/Acme/HelloBundle/Controller/HelloController.php
+++ learnsymfony/src/Acme/HelloBundle/Controller/HelloController.php
@@ -1,13 +1,21 @@
+// src/Acme/HelloBundle/Controller/HelloController.php
 namespace Acme\HelloBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use namespace Acme\HelloBundle\Controller;
-
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HelloController extends Controller
 {
     public function indexAction($name)
     {
-        return new Response('<html><body>Hello '.$name.'!</body></html>');
+        return $this->render(
+            'AcmeHelloBundle:Hello:index.html.twig',
+            array('name' => $name)
+        );
+
+        // render a PHP template instead
+        // return $this->render(
+        //     'AcmeHelloBundle:Hello:index.html.php',
+        //     array('name' => $name)
+        // );
     }
 }
